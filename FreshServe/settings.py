f1138dfa02c11eb6012558269febe627c71c8942
Hritik Valluvar <hxv293@student.bham.ac.uk>
2024-11-26 23:15:44 +0530

    updated config keys

--- FreshServe/settings.py
+++ FreshServe/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y*5exa979)4r63pua)f+xpj$*46)qiqs6m97z7k1agcvueuiw1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '*',
     '.vercel.app',
 ]
 
