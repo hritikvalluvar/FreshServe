c86e23a5704e4b4f5e94354bf7224de6021e76ea
Hritik Valluvar <hxv293@student.bham.ac.uk>
2024-12-03 18:41:19 +0530

    updated config

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Install custom package
+pip install --index-url https://phonepe.mycloudrepo.io/public/repositories/phonepe-pg-sdk-python --extra-index-url https://pypi.org/simple phonepe_sdk==1.1.0
