69e76c2ba577ee8cf257c62b1cd0680db3dd4c5c
Hritik Valluvar <hxv293@student.bham.ac.uk>
2024-12-21 13:40:29 +0530

    fix: callback csrf exempt import

--- customer/views.py
+++ customer/views.py
@@ -8,6 +8,7 @@ from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
 # Local application imports
 from .models import (
